94ae707edbc16bd2d4aa0b55b3bce43abd82e1e7
daishi <arakawa@smoo.co.jp>
2023-04-21 12:46:30 +0900

    initial create code

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-// my-pagination-library.js
+// index.js
+
 class Pagination {
-  constructor(itemsPerPage) {
-    this.itemsPerPage = itemsPerPage;
+  constructor(totalPages, currentPage, baseUrl, maxVisiblePages = 5) {
+    this.totalPages = totalPages;
+    this.currentPage = currentPage;
+    this.baseUrl = baseUrl;
+    this.maxVisiblePages = maxVisiblePages;
   }
 
-  getPage(items, pageNumber) {
-    const start = (pageNumber - 1) * this.itemsPerPage;
-    const end = start + this.itemsPerPage;
-    return items.slice(start, end);
+  render() {
+    let paginationHtml = '';
+
+    if (this.currentPage > 1) {
+      paginationHtml += `<a href="${this.baseUrl}?page=${this.currentPage - 1}">前へ</a>`;
+    }
+
+    if (this.currentPage <= this.maxVisiblePages) {
+      for (let i = 1; i <= Math.min(this.maxVisiblePages, this.totalPages); i++) {
+        paginationHtml += this.pageLink(i);
+      }
+
+      if (this.totalPages > this.maxVisiblePages) {
+        paginationHtml += `...<a href="${this.baseUrl}?page=${this.totalPages}">${this.totalPages}</a>`;
+      }
+    } else if (this.currentPage > this.totalPages - this.maxVisiblePages) {
+      paginationHtml += `<a href="${this.baseUrl}?page=1">1</a>...`;
+
+      for (let i = this.totalPages - this.maxVisiblePages + 1; i <= this.totalPages; i++) {
+        paginationHtml += this.pageLink(i);
+      }
+    } else {
+      paginationHtml += `<a href="${this.baseUrl}?page=1">1</a>...`;
+
+      for (let i = this.currentPage - 2; i <= this.currentPage + 2; i++) {
+        paginationHtml += this.pageLink(i);
+      }
+
+      paginationHtml += `...<a href="${this.baseUrl}?page=${this.totalPages}">${this.totalPages}</a>`;
+    }
+
+    if (this.currentPage < this.totalPages) {
+      paginationHtml += `<a href="${this.baseUrl}?page=${this.currentPage + 1}">次へ</a>`;
+    }
+
+    return paginationHtml;
+  }
+
+  pageLink(page) {
+    if (this.currentPage === page) {
+      return `<span>${page}</span>`;
+    } else {
+      return `<a href="${this.baseUrl}?page=${page}">${page}</a>`;
+    }
   }
 }
 
-const test = "test";
-console.log(test);
+export default Pagination;
