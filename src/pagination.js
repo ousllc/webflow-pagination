57a044bb622a45d690bfba444a333571c6a28965
daishi <arakawa@smoo.co.jp>
2023-04-25 14:20:21 +0900

    link class

--- src/pagination.js
+++ src/pagination.js
@@ -1,79 +1,100 @@
-function splitContentByTag(contentContainer, tag) {
-  const contentParts = [];
-  let tempContainer = document.createElement('div');
-
-  Array.from(contentContainer.children).forEach((child) => {
-    if (child.tagName.toLowerCase() === tag) {
-      if (tempContainer.children.length) {
-        contentParts.push(tempContainer.innerHTML);
-        tempContainer = document.createElement('div');
-      }
-    }
-    tempContainer.appendChild(child.cloneNode(true));
-  });
-
-  contentParts.push(tempContainer.innerHTML);
-
-  return contentParts;
-}
-
-function createPagination(totalPages, currentPage) {
-  let paginationHtml = '';
-
-  if (currentPage > 1) {
-    paginationHtml += `<a href="?page=${currentPage - 1}" class="pagination-prev">前へ</a>`;
-  }
-
-  for (let i = 1; i <= totalPages; i++) {
-    if (i === parseInt(currentPage)) {
-      paginationHtml += `<span class="pagination-count active">${i}</span>`;
-    } else {
-      paginationHtml += `<a href="?page=${i}" class="pagination-count">${i}</a>`;
-    }
-  }
-
-  if (currentPage < totalPages) {
-    paginationHtml += `<a href="?page=${currentPage + 1}" class="pagination-next">次へ</a>`;
-  }
-
-  return paginationHtml;
-}
-
 function webflowPagination(options) {
   document.addEventListener('DOMContentLoaded', () => {
+    // デフォルト設定
     const defaultOptions = {
       splitTag: 'h3',
       contentContainerSelector: '.rich-text-content',
       paginationContainerSelector: '.pagination-container',
+      prevLinkClass: 'pagination-prev',
+      countLinkClass: 'pagination-count',
+      nextLinkClass: 'pagination-next',
     };
 
+    // オプションをマージ
     const settings = { ...defaultOptions, ...options };
 
+    // 現在のページ番号を取得
     const currentPage = new URLSearchParams(window.location.search).get('page') || 1;
+
+    // コンテンツコンテナを取得
     const contentContainer = document.querySelector(settings.contentContainerSelector);
 
     if (contentContainer) {
-      const contentParts = splitContentByTag(contentContainer, settings.splitTag);
+      // コンテンツを指定されたタグで分割する関数
+      function splitContentByTag(tag) {
+        const contentParts = [];
+        let tempContainer = document.createElement('div');
+
+        Array.from(contentContainer.children).forEach((child) => {
+          if (child.tagName.toLowerCase() === tag) {
+            if (tempContainer.children.length) {
+              contentParts.push(tempContainer.innerHTML);
+              tempContainer = document.createElement('div');
+            }
+          }
+          tempContainer.appendChild(child.cloneNode(true));
+        });
+
+        contentParts.push(tempContainer.innerHTML);
+
+        return contentParts;
+      }
+
+      // 分割されたコンテンツを取得
+      const contentParts = splitContentByTag(settings.splitTag);
+
+      // 分割されたコンテンツを表示
       contentContainer.innerHTML = contentParts[currentPage - 1] || '';
 
-      const totalPages = contentParts.length;
-      const paginationHtml = createPagination(totalPages, parseInt(currentPage));
+      // ページネーション機能を実装
+      function createPagination(totalPages, currentPage) {
+        let paginationHtml = '';
 
+        // 前へのリンクを追加
+        if (currentPage > 1) {
+          paginationHtml += `<a href="?page=${currentPage - 1}" class="${settings.prevLinkClass}">前へ</a>`;
+        }
+
+        // ページ番号のリンクを追加
+        for (let i = 1; i <= totalPages; i++) {
+          if (i === parseInt(currentPage)) {
+            paginationHtml += `<span class="${settings.countLinkClass} active">${i}</span>`;
+          } else {
+            paginationHtml += `<a href="?page=${i}" class="${settings.countLinkClass}">${i}</a>`;
+          }
+        }
+
+        // 次へのリンクを追加
+        if (currentPage < totalPages) {
+          paginationHtml += `<a href="?page=${currentPage + 1}" class="${settings.nextLinkClass}">次へ</a>`;
+        }
+
+        return paginationHtml;
+      }
+
+
+      // ページネーションHTMLを生成
+      const totalPages = contentParts.length;
+      let paginationHtml = createPagination(totalPages, parseInt(currentPage));
+
+      // ページネーシーションHTMLを挿入
       const paginationContainer = document.querySelector(settings.paginationContainerSelector);
       paginationContainer.innerHTML = paginationHtml;
 
+      // クリックイベントを追加
       paginationContainer.addEventListener('click', (event) => {
         if (event.target.classList.contains('pagination-ellipsis')) {
           event.preventDefault();
           const startPage = parseInt(event.target.getAttribute('data-start-page'));
-          const updatedPaginationHtml = createPagination(totalPages, parseInt(currentPage), startPage);
-          paginationContainer.innerHTML = updatedPaginationHtml;
+          paginationHtml = createPagination(totalPages, parseInt(currentPage), startPage);
+          paginationContainer.innerHTML = paginationHtml;
         }
+        // 前のページ部分に戻るリンクのクリックイベント
         if (event.target.classList.contains('pagination-ellipsis-back')) {
           event.preventDefault();
           const startPage = parseInt(event.target.getAttribute('data-start-page'));
-          const updatedPaginationHtml = createPagination(totalPages, parseInt(currentPage), startPage);
-          paginationContainer.innerHTML = updatedPaginationHtml;
+          paginationHtml = createPagination(totalPages, parseInt(currentPage), startPage);
+          paginationContainer.innerHTML = paginationHtml;
         }
       });
     }
