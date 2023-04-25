67738f1146f750f5f1f116dc4a027a11efbffcc2
daishi <arakawa@smoo.co.jp>
2023-04-25 12:34:56 +0900

    bug fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 // rollup.config.js
 import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'src/pagination.js', // 入力ファイルのパス
   output: {
-    file: 'dist/pagination.min.js', // 出力ファイルのパス
-    format: 'umd', // モジュール形式（適宜変更可能）
-    name: 'Pagination' // ライブラリ名（適宜変更可能）
+    file: 'dist/pagination.min.js',
+    format: 'iife',
+    name: 'webflowPagination',
+    sourcemap: true,
   },
-  plugins: [terser()] // terserプラグインを適用
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ]
 };
